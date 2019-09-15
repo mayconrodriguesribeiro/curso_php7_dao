113c3b6760ed17389b21d9e7ca6139ecad63edfe
mayconribeiro@live.com <mayconribeiro@live.com>
2019-09-15 15:54:55 -0300

    Inserido Classe primeiros e metodos

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadById(4);
 
-$root->loadById(4);
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//busca um usuario peloparametro informado
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//busca um login  emostra o usuario dele
+$usuario = new Usuario();
+$usuario->login("segundo","segundo");
+
+echo $usuario;
 
 ?>
